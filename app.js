fe3c1ebf1862e0406ff104463f1a2e0b891f7d1d
Matias Ikonen <alcatel1962@outlook.com>
2022-03-19 11:21:25 +0100

    added some comments

--- app.js
+++ app.js
@@ -29,6 +29,7 @@ function playRound(playerSelection, computerSelection) {
       }
 }
 
+// Check winner and log the winner with score
 function checkWinner() {
   if (computerScore === playerScore) {
     console.log("Its tie!");
